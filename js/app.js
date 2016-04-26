55704239fa8b23fca95e163073e684e4b17dd549
Jagaa <jagaa1103@gmail.com>
2016-04-26 23:38:06 +0200

    get persons.json

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 var demo = angular.module('demo', []);
 
+demo.controller('mainCtrl', function($scope, $http){
+	init();
+	function init(){
+		$http.get('persons.json').success(function(res){
+			console.log(res);
+		}).error(function(err){
+			alert(err);
+		})
+	}
+});
 
