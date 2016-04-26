8d39215751584edf75cee71fef8a7b42cefcae83
Jagaa <jagaa1103@gmail.com>
2016-04-26 23:46:30 +0200

    added dump textarea and show list of persons

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
 var demo = angular.module('demo', []);
 
 demo.controller('mainCtrl', function($scope, $http){
+	
 	init();
 	function init(){
 		$http.get('persons.json').success(function(res){
 			console.log(res);
+			$scope.persons = res;
 		}).error(function(err){
 			alert(err);
 		})
 	}
+
+	$scope.$watch('persons', function(){
+		$scope.dump = JSON.stringify($scope.persons);
+	})
 });
 
